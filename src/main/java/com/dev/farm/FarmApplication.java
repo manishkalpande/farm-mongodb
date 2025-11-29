b1203c77b88c064ad7e6f70e54934e8a8f08625d
manishkalpande <manishkalapnde23@gmail.com>
2025-11-29 12:12:48 +0530

    h2 databse

--- src/main/java/com/dev/farm/FarmApplication.java
+++ src/main/java/com/dev/farm/FarmApplication.java
@@ -2,8 +2,10 @@ package com.dev.farm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FarmApplication {
 
 	public static void main(String[] args) {
