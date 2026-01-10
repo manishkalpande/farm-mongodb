832911c922cd121428811b8fc5afa9620330dbe6
manishkalpande <manishkalpande23@gmail.com>
2026-01-10 12:57:58 +0530

    update

--- src/main/java/com/dev/farm/controller/UserController.java
+++ src/main/java/com/dev/farm/controller/UserController.java
@@ -1,32 +1,32 @@
-package com.dev.farm.controller;
-
-
-import com.dev.farm.entity.User;
-import com.dev.farm.service.UserRedis;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/user")
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserRedis repository;
-
-    @PostMapping
-    public String save(@RequestBody User user) {
-        repository.save(user);
-        return "User saved!";
-    }
-
-    @GetMapping("/{id}")
-    public User get(@PathVariable String id) {
-        return repository.findById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable String id) {
-        repository.delete(id);
-        return "User deleted!";
-    }
-}
+//package com.dev.farm.controller;
+//
+//
+//import com.dev.farm.entity.User;
+//import com.dev.farm.service.UserRedis;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/user")
+//@RequiredArgsConstructor
+//public class UserController {
+//
+//    private final UserRedis repository;
+//
+//    @PostMapping
+//    public String save(@RequestBody User user) {
+//        repository.save(user);
+//        return "User saved!";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public User get(@PathVariable String id) {
+//        return repository.findById(id);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable String id) {
+//        repository.delete(id);
+//        return "User deleted!";
+//    }
+//}
--- src/main/java/com/dev/farm/service/UserRedis.java
+++ src/main/java/com/dev/farm/service/UserRedis.java
@@ -1,27 +1,27 @@
-package com.dev.farm.service;
-
-
-import com.dev.farm.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@RequiredArgsConstructor
-public class UserRedis {
-
-    private final RedisTemplate<String, Object> redisTemplate;
-    private static final String KEY = "USER";
-
-    public void save(User user) {
-        redisTemplate.opsForHash().put(KEY, user.getId(), user);
-    }
-
-    public User findById(String id) {
-        return (User) redisTemplate.opsForHash().get(KEY, id);
-    }
-
-    public void delete(String id) {
-        redisTemplate.opsForHash().delete(KEY, id);
-    }
-}
+//package com.dev.farm.service;
+//
+//
+//import com.dev.farm.entity.User;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//@RequiredArgsConstructor
+//public class UserRedis {
+//
+//    private final RedisTemplate<String, Object> redisTemplate;
+//    private static final String KEY = "USER";
+//
+//    public void save(User user) {
+//        redisTemplate.opsForHash().put(KEY, user.getId(), user);
+//    }
+//
+//    public User findById(String id) {
+//        return (User) redisTemplate.opsForHash().get(KEY, id);
+//    }
+//
+//    public void delete(String id) {
+//        redisTemplate.opsForHash().delete(KEY, id);
+//    }
+//}
