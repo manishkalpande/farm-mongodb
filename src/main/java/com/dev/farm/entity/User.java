eb865de3c66e081948e4bff0d0f0e5ef16ca18f0
manishkalpande <manishkalpande23@gmail.com>
2026-01-10 22:42:28 +0530

    add mongodb code

--- src/main/java/com/dev/farm/entity/User.java
+++ src/main/java/com/dev/farm/entity/User.java
@@ -2,14 +2,14 @@ package com.dev.farm.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@Entity
-@Table(name = "users")
+@Document(collection = "users")
 @Setter
 @Getter
 @AllArgsConstructor
--- src/main/java/com/dev/farm/repository/UserRepository.java
+++ src/main/java/com/dev/farm/repository/UserRepository.java
@@ -3,12 +3,13 @@ package com.dev.farm.repository;
 import com.dev.farm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Long> {
 
 //    @Query("select e from User ")
     Optional<User> findByUsername(String username);
